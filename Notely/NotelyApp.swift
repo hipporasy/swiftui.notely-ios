008d5ad5da92c7f8f405f5a298957c0917716c6f
Marasy Phi <hipporasy@gmail.com>
2021-01-09 13:59:01 +0700

    Initial Commit

---
+++ Notely/NotelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotelyApp.swift
+//  Notely
+//
+//  Created by Marasy Phi on 9/1/21.
+//
+
+import SwiftUI
+
+@main
+struct NotelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
